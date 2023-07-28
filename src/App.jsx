e250f19a19e086d2dae1d91d4a938cdbced3f06a
zednegr <negrzed@gmail.com>
2023-07-28 11:48:28 +0500

    last update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import Header from './components/header/header'
 import Home from './pages/home'
 import Contact from './pages/contact'
@@ -9,19 +9,15 @@ function App() {
 
   return (
     <>
+      <Header />
 
-      <BrowserRouter>
-
-        <Header />
-
-        <div className="container">
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/contact' element={<Contact />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
+      <div className="container">
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </div>
     </>
   )
 }
--- src/components/imgs/img.jsx
+++ src/components/imgs/img.jsx
@@ -35,17 +35,17 @@ function Img() {
         <section>
             <div className="cards">
                 {
-                        <div className="card">
-                            <div className="card-top">
-                                <img className="cat-img" src={getData?.message} width={'200px'} height={'200px'} alt="" />
-                            </div>
-                            <div className="card-about">
-                                <p className="card-about-p">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque, delectus!</p>
-                            </div>
-                            <div className="card-middle">
-                                <a className="next-btn btn-slide" href="">Next</a>
-                            </div>
+                    <div className="card">
+                        <div className="card-top">
+                            <img className="cat-img" src={getData?.message} width={'200px'} height={'200px'} alt="" />
                         </div>
+                        <div className="card-about">
+                            <p className="card-about-p">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque, delectus!</p>
+                        </div>
+                        <div className="card-middle">
+                            <a className="next-btn btn-slide" href="">Next</a>
+                        </div>
+                    </div>
                 }
             </div>
         </section>
